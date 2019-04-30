14a08a53ef8fe60a8efc9283050e65e925696158
Andrew Bay <daddnbay@yahoo.com>
2019-04-30 15:39:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,7 +25,9 @@ end
 
 #Array of integers and returns a copy of the arryay with the elements in reverse order
 def reverse_array(array)
-  array.sort {|a,b| array.reverse}
+  array.sort do |a,b|
+    array.reverse
+  end
 end
   
 
