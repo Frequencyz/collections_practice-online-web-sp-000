82688a73f8f6f443f24e45be668d6178b6839dc9
Andrew Bay <daddnbay@yahoo.com>
2019-04-30 15:23:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,23 +1,23 @@
-#Array of integers in ascending order
+# Array of integers in ascending order
 def sort_array_asc(array)
 array.sort do |a,b|
   a <=> b
  end
 end
 
-#Array of integers in descending order
+# Array of integers in descending order
 def sort_array_desc(array)
   array.sort do |a,b|
     b <=> a
   end
 end
 
-#Array with the strings ordered in ascending order by length 
+# Array with the strings ordered in ascending order by length 
 def sort_array_char_count(array)
   array.sort {|a,b| a.length <=> b.length}
 end
 
-#Takes in an array and swaps the second and third elements
+# Takes in an array and swaps the second and third elements
 
 
   
