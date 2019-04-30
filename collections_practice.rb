308867161152f3e8d7a928ed7ed57ee8298969f2
Andrew Bay <daddnbay@yahoo.com>
2019-04-30 15:08:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,12 +1,13 @@
 # Array of integers in ascending order
-def sort_array_asc (array)
+def sort_array_asc(array)
 array.sort do |a,b|
   a <=> b
  end
 end
 
 # Array of integers in descending order
-def sort_array_desc
+def sort_array_desc(array)
+  array.sort do |
 
 
 
