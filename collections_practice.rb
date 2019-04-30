42440f89817d5d3671db3327b275f1228cfe3f39
Andrew Bay <daddnbay@yahoo.com>
2019-04-30 15:11:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,16 +1,21 @@
-# Array of integers in ascending order
+#Array of integers in ascending order
 def sort_array_asc(array)
 array.sort do |a,b|
   a <=> b
  end
 end
 
-# Array of integers in descending order
+#Array of integers in descending order
 def sort_array_desc(array)
   array.sort do |a,b|
     b <=> a
   end
 end
+
+#Array with the strings ordered in ascending order by length 
+def sort_array_char_count(array)
+  
+  
 
 
 
